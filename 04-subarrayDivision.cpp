d131c39fb170d7b03e2fa082460e5eb4a453a3fd
SaiSudheerReddy <62324377+SaiSudheerRedddy@users.noreply.github.com>
2021-09-25 16:09:51 +0530

    Update 04-subarrayDivision.cpp

--- 04-subarrayDivision.cpp
+++ 04-subarrayDivision.cpp
@@ -1,5 +1,7 @@
 // Link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
+
+// Solution for O(n^2)
 int birthday(vector<int> s, int d, int m) {
     int count = 0;
     for(int i=0;i<s.size();i++){
@@ -14,4 +16,30 @@ int birthday(vector<int> s, int d, int m) {
         }
     }
     return count;
+}
+
+
+// Optimised way with O(n)
+
+int birthday(vector<int> s, int d, int m) {
+    int count = 0,sum = 0;
+    for(int i=0;i<m;i++){
+        sum += s[i];
+    }
+    if(d == 1 and sum == d){
+        count ++;
+        return count;
+    }
+    if(sum == d){
+        count ++;
+    }
+    
+    for(int i=m; i<s.size(); i++){
+        sum = sum + s[i] - s[i - m];
+        if(sum == d){
+            count ++;
+        }
+    }
+    return count;
+    
 }
