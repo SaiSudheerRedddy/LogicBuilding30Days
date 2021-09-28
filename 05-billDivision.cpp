d52d0a4f3d7c21ba7865eb3ec6139264ac116885
SaiSudheerReddy <62324377+SaiSudheerRedddy@users.noreply.github.com>
2021-09-28 13:34:21 +0530

    Update 05-billDivision.cpp

--- 05-billDivision.cpp
+++ 05-billDivision.cpp
@@ -1,1 +1,13 @@
+// Link: https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
 
+void bonAppetit(vector<int> bill, int k, int b) {
+    int sum = 0;
+    for(int i=0;i<bill.size();i++){
+        sum += bill[i];
+    }
+    if(b == ((sum - bill[k])/2)){
+        cout<<"Bon Appetit"<<endl;
+    }else{
+        cout<<(b - ((sum - bill[k])/2))<<endl;
+    }
+}
